ba3a48ec53ba2d3412620687d002e072051fa62e
qwerty <qwerty@DESKTOP-9MDF0U1>
2024-06-12 08:45:50 +0300

    Добавьте файлы проекта.

---
+++ To_book_Hotel/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_book_Hotel
+{
+    public class UserData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int NumberOfPeople { get; set; }
+        public string CheckInDate { get; set; }
+        public string CheckOutDate { get; set; }
+    }
+}
---
+++ To_book_Hotel/MenWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+namespace To_book_Hotel
+{
+    public partial class MenWindow
+    {
+        public MenWindow()
+        {
+            InitializeComponent();
+            calendar.SelectedDatesChanged += Calendar_SelectedDatesChanged;
+            btn.Click += Button_Click;
+            btn2.Click += Button_Click_1; // Добавляем обработчик события для второй кнопки
+        }
+        private DateTime startDate;
+        private DateTime endDate;
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (calendar.SelectedDates.Count > 0)
+            {
+                startDate = calendar.SelectedDates[0];
+                date.Text = startDate.ToString("dd/MM/yyyy");
+            }
+
+            if (calendar2.SelectedDates.Count > 1)
+            {
+                endDate = calendar2.SelectedDates[1];
+                date2.Text = endDate.ToString("dd/MM/yyyy");
+            }
+
+            date.Text = startDate.ToString("dd/MM/yyyy");
+            date2.Text = endDate.ToString("dd/MM/yyyy");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var selectedDate = calendar.SelectedDate;
+            if (selectedDate.HasValue)
+            {
+                date.Text = selectedDate.Value.ToString("dd.MM.yyyy");
+
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            var selectedDate = calendar2.SelectedDate;
+            if (selectedDate.HasValue)
+            {
+                date2.Text = selectedDate.Value.ToString("dd.MM.yyyy");
+
+
+            }
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // Создаем экземпляр UserData и заполняем данными из TextBox'ов
+            UserData userData = new UserData
+            {
+                FirstName = name.Text,
+                LastName = lastname.Text,
+                NumberOfPeople = int.Parse(people.Text),
+                CheckInDate = date.Text,
+                CheckOutDate = date2.Text
+            };
+            string json = JsonConvert.SerializeObject(userData, Formatting.Indented);
+
+
+            File.WriteAllText("UserData.json", json);
+
+            MessageBox.Show("Данные успешно сохранены!");
+        }
+    }
+}
